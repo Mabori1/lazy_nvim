1fd34594c789da8465c0e38aeeda4fcf97bf5855
Alexey_Makeev <makeevhome@mail.ru>
2023-06-13 13:35:29 +0300

    delete copilot

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -42,6 +42,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
+
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
